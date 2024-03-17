49e525a4d47115b98459199995591f1aacef5131
Walker Aldridge <daldridge@lairdscomputer.com>
2024-03-17 09:34:16 -0500

    Refactor upload_file, bump version

--- src/waifuvault/waifuvault.py
+++ src/waifuvault/waifuvault.py
@@ -12,44 +12,31 @@ from .waifumodels import FileResponse, FileUpload
 
 # Upload File
 def upload_file(file_obj: FileUpload):
-    parameters = {}
-    if file_obj.password:
-        parameters['password'] = file_obj.password
-    if file_obj.expires:
-        parameters['expires'] = file_obj.expires
-    if file_obj.hidefilename:
-        parameters['hide_filename'] = file_obj.hidefilename
+    parameters = __build_parameters(file_obj)
 
     if file_obj.is_buffer():
         multipart_data = MultipartEncoder(
-            fields={
-                'file': (file_obj.target_name, file_obj.target)
-            }
+            fields={'file': (file_obj.target_name, file_obj.target)}
         )
-        response = requests.put(
-            __base_url__,
-            params=parameters,
-            data=multipart_data,
-            headers={'Content-Type': multipart_data.content_type})
+        header_data = {'Content-Type': multipart_data.content_type}
     elif file_obj.is_url():
-        response = requests.put(
-            __base_url__,
-            params=parameters,
-            data={'url': file_obj.target}
-        )
+        multipart_data = {'url': file_obj.target}
+        header_data = None
     else:
         multipart_data = MultipartEncoder(
             fields={
                 'file': (os.path.basename(file_obj.target), open(file_obj.target, 'rb'))
             }
         )
-        response = requests.put(
-            __base_url__,
-            params=parameters,
-            data=multipart_data,
-            headers={'Content-Type': multipart_data.content_type})
-    check_error(response, False)
-    return dict_to_obj(json.loads(response.text))
+        header_data = {'Content-Type': multipart_data.content_type}
+
+    response = requests.put(
+        __base_url__,
+        params=parameters,
+        data=multipart_data,
+        headers=header_data)
+    __check_error(response, False)
+    return __dict_to_obj(json.loads(response.text))
 
 
 # Get File Info
@@ -59,15 +46,15 @@ def file_info(token: str, formatted: bool):
         url,
         params={'formatted': 'true' if formatted else 'false'}
     )
-    check_error(response, False)
-    return dict_to_obj(json.loads(response.text))
+    __check_error(response, False)
+    return __dict_to_obj(json.loads(response.text))
 
 
 # Delete File
 def delete_file(token: str):
     url = f"{__base_url__}/{token}"
     response = requests.delete(url)
-    check_error(response, False)
+    __check_error(response, False)
     return True if response.text == "true" else False
 
 
@@ -81,12 +68,12 @@ def get_file(file_obj: FileResponse, password: str = None):
     else:
         url = file_obj.url
     response = requests.get(url, headers=headers)
-    check_error(response, True)
+    __check_error(response, True)
     return BytesIO(response.content)
 
 
 # Check Error
-def check_error(response: requests.models.Response, is_download: bool):
+def __check_error(response: requests.models.Response, is_download: bool):
     if not response.ok:
         try:
             err = json.loads(response.text)
@@ -94,16 +81,27 @@ def check_error(response: requests.models.Response, is_download: bool):
             name = err["name"]
             message = err['message']
         except:
-            status = "Password is Incorrect" if response.status_code == 403 and is_download else response.text
-            name = ""
-            message = ""
+            status = response.status_code
+            name = "Password is Incorrect" if response.status_code == 403 and is_download else response.status_code
+            message = response.text
         raise Exception(f"Error {status} ({name}): {message}")
     return
 
 
-def dict_to_obj(dict_obj: any):
+def __build_parameters(file_obj: FileUpload):
+    parameters = {}
+    if file_obj.password:
+        parameters['password'] = file_obj.password
+    if file_obj.expires:
+        parameters['expires'] = file_obj.expires
+    if file_obj.hidefilename:
+        parameters['hide_filename'] = file_obj.hidefilename
+    return parameters
+
+
+def __dict_to_obj(dict_obj: any):
     return FileResponse(
         dict_obj["token"],
         dict_obj["url"],
-        True if dict_obj["protected"] == "true" else False,
+        dict_obj["protected"] == "true",
         dict_obj["retentionPeriod"])
