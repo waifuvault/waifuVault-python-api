4da318dc7b7c6a4beeb9061837eb70bcdcabc7d2
Walker Aldridge <daldridge@lairdscomputer.com>
2024-03-23 10:27:37 -0500

    Add negative tests

--- tests/test_waifuvault.py
+++ tests/test_waifuvault.py
@@ -1,12 +1,15 @@
 import io
+import re
 
+import pytest
 import waifuvault
 
 
 # Response Mock
 class response_mock:
-    def __init__(self,ok,text,content=None):
+    def __init__(self, ok, text, content=None, code=None):
         self.ok = ok
+        self.code = code
         self.text = text
         self.content = content
 
@@ -29,6 +32,15 @@ def test_upload_url(mocker):
     assert (upload_res.retentionPeriod == 100), "Retention does not match"
 
 
+def test_upload_url_error(mocker):
+    mock_put = mocker.patch('requests.put',
+        return_value = response_mock(False,
+                '{"name": "BAD_REQUEST", "message": "Error Test", "status": 400}',code=400))
+    upload_file = waifuvault.FileUpload("https://walker.moe/assets/sunflowers.png", expires="10m")
+    with pytest.raises(Exception, match=re.escape('Error 400 (BAD_REQUEST): Error Test')):
+        upload_res = waifuvault.upload_file(upload_file)
+
+
 def test_upload_file(mocker):
     mock_put = mocker.patch('requests.put',
         return_value = response_mock(True,
@@ -40,6 +52,15 @@ def test_upload_file(mocker):
     assert (upload_res.token == "test-token"), "Token does not match"
     assert (upload_res.protected is False), "Protected does not match"
     assert (upload_res.retentionPeriod == 100), "Retention does not match"
+
+
+def test_upload_file_error(mocker):
+    mock_put = mocker.patch('requests.put',
+        return_value = response_mock(False,
+                '{"name": "BAD_REQUEST", "message": "Error Test", "status": 400}',code=400))
+    upload_file = waifuvault.FileUpload("tests/testfile.png", expires="10m")
+    with pytest.raises(Exception, match=re.escape('Error 400 (BAD_REQUEST): Error Test')):
+        upload_res = waifuvault.upload_file(upload_file)
 
 
 def test_upload_buffer(mocker):
@@ -56,6 +77,18 @@ def test_upload_buffer(mocker):
     assert (upload_res.protected is False), "Protected does not match"
     assert (upload_res.retentionPeriod == 100), "Retention does not match"
 
+
+def test_upload_buffer_error(mocker):
+    mock_put = mocker.patch('requests.put',
+        return_value = response_mock(False,
+                '{"name": "BAD_REQUEST", "message": "Error Test", "status": 400}',code=400))
+    with open("tests/testfile.png", "rb") as fh:
+        buf = io.BytesIO(fh.read())
+    upload_file = waifuvault.FileUpload(buf, "testfile_buf.png", expires="10m")
+    with pytest.raises(Exception, match=re.escape('Error 400 (BAD_REQUEST): Error Test')):
+        upload_res = waifuvault.upload_file(upload_file)
+
+
 def test_file_info(mocker):
     mock_get = mocker.patch('requests.get',
         return_value=response_mock(True,
@@ -68,6 +101,14 @@ def test_file_info(mocker):
     assert (upload_info.token == "test-token"), "Token does not match"
     assert (upload_info.protected is False), "Protected does not match"
     assert (upload_info.retentionPeriod == "10 minutes"), "Retention does not match"
+
+
+def test_file_info_error(mocker):
+    mock_get = mocker.patch('requests.get',
+            return_value=response_mock(False,
+                                       '{"name": "BAD_REQUEST", "message": "Error Test", "status": 400}', code=400))
+    with pytest.raises(Exception, match=re.escape('Error 400 (BAD_REQUEST): Error Test')):
+        upload_info = waifuvault.file_info("bad-token",True)
 
 
 def test_update_info(mocker):
@@ -84,6 +125,14 @@ def test_update_info(mocker):
     assert (update_info.retentionPeriod == 100), "Retention does not match"
 
 
+def test_update_info_error(mocker):
+    mock_patch = mocker.patch('requests.patch',
+                              return_value=response_mock(False,
+                                                         '{"name": "BAD_REQUEST", "message": "Error Test", "status": 400}', code=400))
+    with pytest.raises(Exception, match=re.escape('Error 400 (BAD_REQUEST): Error Test')):
+        update_info = waifuvault.file_update("test-token","dangerWaifu")
+
+
 def test_delete(mocker):
     mock_del = mocker.patch('requests.delete',
         return_value=response_mock(True,
@@ -93,8 +142,24 @@ def test_delete(mocker):
     assert (del_file is True), "Delete did not return true"
 
 
+def test_delete_error(mocker):
+    mock_del = mocker.patch('requests.delete',
+                            return_value=response_mock(False,
+                                                       '{"name": "BAD_REQUEST", "message": "Error Test", "status": 400}', code=400))
+    with pytest.raises(Exception, match=re.escape('Error 400 (BAD_REQUEST): Error Test')):
+        del_file = waifuvault.delete_file("test-token")
+
+
 def test_download(mocker):
     mock_get = mocker.patch('requests.get',return_value = response_mock(True,'', bytes("someval","utf8")))
     file_down = waifuvault.get_file(waifuvault.FileResponse(url="https://waifuvault.moe/f/something"), "dangerWaifu")
     mock_get.assert_called_once_with('https://waifuvault.moe/f/something', headers={'x-password': 'dangerWaifu'})
     assert (isinstance(file_down, io.BytesIO)), "Download did not return a buffer"
+
+
+def test_download_error(mocker):
+    mock_get = mocker.patch('requests.get',
+                            return_value=response_mock(False,
+                                                       '{"name": "BAD_REQUEST", "message": "Error Test", "status": 400}', code=400))
+    with pytest.raises(Exception, match=re.escape('Error 400 (BAD_REQUEST): Error Test')):
+        file_down = waifuvault.get_file(waifuvault.FileResponse(url="https://waifuvault.moe/f/something"), "dangerWaifu")
