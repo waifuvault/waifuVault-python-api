cc3d5f474be7d44d6c206e5043a98650069a9ecc
Walker Aldridge <daldridge@lairdscomputer.com>
2025-01-30 19:47:34 -0600

    Add set_alt_baseurl to init

--- src/waifuvault/__init__.py
+++ src/waifuvault/__init__.py
@@ -2,4 +2,5 @@ from .waifumodels import (FileResponse, FileUpload, BucketResponse, Restriction,
                           AlbumResponse)
 from .waifuvault import (upload_file, file_info, get_file, delete_file, file_update, create_bucket, get_bucket,
                          delete_bucket, get_restrictions, clear_restrictions, get_file_stats, create_album, delete_album,
-                         get_album, associate_file, disassociate_file, share_album, revoke_album, download_album)
+                         get_album, associate_file, disassociate_file, share_album, revoke_album, download_album,
+                         set_alt_baseurl)
